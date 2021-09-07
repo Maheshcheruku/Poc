6b4ecbcb1bcc605e859a4d69d611d6d1cd790380
Maheshcheruku <ram.mahesh88@gmail.com>
2021-09-07 18:04:15 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  backend "azurerm" {
-    subscription_id      = "fcedf982-5b4a-44d2-b26d-9aa11cebcddf"
-    tenant_id            = "ec27f9db-755f-4aa0-b06f-5f66f3d6e833"  
-    resource_group_name  = "rg-terraformstate"
-    storage_account_name = "terrastatestorage2134"
-    container_name       = "terraformdemo"
-    key                  = "dev.terraform.tfstate"
-  }
-}
-
 #configure azurerm provider
 provider "azurerm" {
   version = "1.38"
