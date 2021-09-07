bca9271eb16eea3eaa37a802f8d08dc20fe99aee
Maheshcheruku <ram.mahesh88@gmail.com>
2021-09-07 23:03:56 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,34 +1,17 @@
+#Set up remote state
 terraform {
-    required_providers  {
-        azurerm =   {
-            source  =   "hashicorp/azurerm"
-        }
-    }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraformstate"
+    storage_account_name = "terrastatestorage2134"
+    container_name       = "terraformdemo"
+    key                  = "dev.terraform.tfstate"
+  }
 }
 
-# Provider Block
-
+#configure azurerm provider
 provider "azurerm" {
-    version         =   "~> 2.0"
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    subscription_id =   var.subscription_id
-    tenant_id       =   var.tenant_id
-    
-    features {}
+  version = "1.38"
 }
-
-
-
-
-provider "azuread" {
-    version         =   ">= 0.11"
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    tenant_id       =   var.tenant_id
-    alias           =   "ad"
-}
-
 
 #create resource group
 resource "azurerm_resource_group" "rg" {
