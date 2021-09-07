5eec61cbec1246392069cfca53ae75e5fd9664b9
Maheshcheruku <ram.mahesh88@gmail.com>
2021-09-07 22:52:53 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,7 +1,34 @@
-#configure azurerm provider
-provider "azurerm" {
-  version = "1.38"
+terraform {
+    required_providers  {
+        azurerm =   {
+            source  =   "hashicorp/azurerm"
+        }
+    }
 }
+
+# Provider Block
+
+provider "azurerm" {
+    version         =   "~> 2.0"
+    client_id       =   var.client_id
+    client_secret   =   var.client_secret
+    subscription_id =   var.subscription_id
+    tenant_id       =   var.tenant_id
+    
+    features {}
+}
+
+
+
+
+provider "azuread" {
+    version         =   ">= 0.11"
+    client_id       =   var.client_id
+    client_secret   =   var.client_secret
+    tenant_id       =   var.tenant_id
+    alias           =   "ad"
+}
+
 
 #create resource group
 resource "azurerm_resource_group" "rg" {
